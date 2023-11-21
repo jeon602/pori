d4b22399897de9d78b264bb7a0808870db045b45
jeon602 <731aa@naver.com>
2023-11-21 17:39:27 +0900

    initial settings

---
+++ src/main/java/com/example/pori/PoriApplication.java
@@ -0,0 +1,13 @@
+package com.example.pori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoriApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoriApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pori/PoriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoriApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
